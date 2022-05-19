fca3a084d5d49d6373f7b190cf8815b99eedb1fc
FabKe91 <fabian.keller.mail@gmail.com>
2022-05-20 00:29:09 +0200

    functional script to write parameters

--- parameters/create_fitparameterfile.py
+++ parameters/create_fitparameterfile.py
@@ -13,7 +13,7 @@ PLLIST = ["DPPC", "DUPC"]
 CHOLNAME = "CHL1"
 
 PLPLPAIRS = ["DPPC_DPPC", "DUPC_DUPC", "DUPC_DPPC"]
-PLCPAIRS = ["DPPC_CHL1", "DUPC_CHL1", "DUPC_CHL1"]
+PLCPAIRS = ["DPPC_CHL1", "DUPC_CHL1"]
 
 #######################################
 
@@ -28,7 +28,6 @@ def _read_logistic_parafile(df):
      * @param[in] S function variable
      */
     '''
-    print(df)
     growth = df.growth.iloc[0]
     Egain = df.Egain.iloc[0]
     Emax = df.Emax.iloc[0]
@@ -46,12 +45,11 @@ def _read_sigmoid_parafile(df):
          * @param[in] T function variable
          */
     '''
-    print(df)
     growth = df.growth.iloc[0]
     deltaN = df.deltaN.iloc[0]
     Nmax = df.Nmax.iloc[0]
-    transitionT = df.transitionT
-    return ' '.join([str(Emax), str(Egain), str(growth), str(S0) ])
+    transitionT = df.transitionT.iloc[0]
+    return ' '.join([str(Nmax), str(deltaN), str(growth), str(transitionT) ])
 
 def _read_lennard_parafile(df):
     '''
@@ -64,7 +62,6 @@ def _read_lennard_parafile(df):
      * @param[in] S function variable
      */
     '''
-    print(df)
     deg = df.deg.iloc[0] #=k
     E0 = df.E0.iloc[0]
     S0 = df.S0.iloc[0]
@@ -74,6 +71,7 @@ def _read_lennard_parafile(df):
 def _read_poly_parafile(df):
     ''' for polynomials of the for \sumi=0 coeff[i]*x**i '''
     print("Warning: polynomial coefficients must be given in ascending order")
+    print("data okay?\n", df)
     return ' '.join([str(df[col].iloc[0]) for col in df.columns])
 
 
@@ -92,6 +90,12 @@ def add_Eplpl(f, parafilename="fitparameters_EPLPL_Nc.txt"):
      * @param[in] S function variable
      */
     '''
+    print("reading: ", parafilename)
+
+    f.write("#logistic function - Eplpl(Smin, Nc):\n")
+    f.write("# f(S) = Emax + ( Egain / (1+exp(-k(S-S0))) )\n")
+    f.write("# <Emax> <Egain> <k> <S0>\n")
+
 
     sep = "\t"
 
@@ -105,14 +109,13 @@ def add_Eplpl(f, parafilename="fitparameters_EPLPL_Nc.txt"):
         print("Failed to read Eplpl file")
         raise ValueError(f"Separator {sep} incorrect")
 
-    print(dat)
     for plpair in PLPLPAIRS:
         for Nc in range(7):
             df = dat.loc[(dat.cholconc == CHOLCONC) & (dat.pair == plpair) & (dat.Nc == Nc)]
-            print(df)
             outstr = ' '.join(["Enthalpy", plpair.replace("_"," "), str(Nc), _read_logistic_parafile(df), "\n"]) 
             f.write(outstr)
 
+    f.write("\n")
 
 def add_Eplc(f, parafilename="fitparameters_EPLC_Nc.txt"):
     '''
@@ -126,6 +129,11 @@ def add_Eplc(f, parafilename="fitparameters_EPLC_Nc.txt"):
      */
     '''
     sep = "\t"
+
+    f.write("#lennard jones like - Eplc(Shost, Nc):\n")
+    f.write("# Q = [(S0 - Sshift) / (S - Sshift)]**k\n")
+    f.write("# f(S) = -E0 * Q * (Q - 2)\n")
+    f.write("#\t\t\t<S0> <Sshift> <k> <E0>\n")
 
     try:
         dat = pd.read_csv(parafilename, sep=sep)
@@ -142,13 +150,17 @@ def add_Eplc(f, parafilename="fitparameters_EPLC_Nc.txt"):
             df = dat.loc[(dat.cholconc == CHOLCONC) & (dat.pair == plcpair) & (dat.Nc == Nc)]
             outstr = ' '.join(["LipidCholEnergie", plcpair.replace(f"_{CHOLNAME}", ""), str(Nc), _read_lennard_parafile(df), "\n"]) 
             f.write(outstr)
+    f.write("\n")
 
 
 
 
-def add_Ecc(f, parafilename="fitparameters_CC_Nc.txt"):
+def add_Ecc(f, parafilename="fitparameters_ECC_Nc.txt"):
     ''' linear fit '''
     sep = "\t"
+    print("reading: ", parafilename)
+
+    f.write("#linear - Ecc(Nc):\n")
 
     try:
         dat = pd.read_csv(parafilename, sep=sep)
@@ -164,10 +176,15 @@ def add_Ecc(f, parafilename="fitparameters_CC_Nc.txt"):
         df = dat.loc[(dat.cholconc == CHOLCONC) & (dat.PL == pl)].drop(columns=["cholconc", "PL"])
         outstr = ' '.join(["CholCholEnergie", pl, _read_poly_parafile(df), "\n"]) 
         f.write(outstr)
+    f.write("\n")
 
 
 def add_selfE(f, parafilename="fitparameters_selfE.txt"):
     ''' polynomial fit '''
+    print("reading: ", parafilename)
+
+    f.write("#polynomial - self interaction Eself(S):\n")
+
     sep = "\t"
 
     try:
@@ -185,14 +202,88 @@ def add_selfE(f, parafilename="fitparameters_selfE.txt"):
         outstr = ' '.join(["SelfEnergie", pl, _read_poly_parafile(df), "\n"]) 
         f.write(outstr)
 
+    f.write("\n")
 
-def add_NLL():
+def add_NLL(f, lipidtype, parafilename="fitparameters_NofT_Nc_{}.txt"):
+    ''' fit depends on lipid type '''
+
+    parafilename = parafilename.format(lipidtype)
+    print("reading: ", parafilename)
+
+    f.write("#Lipid-Lipid neighbor functions sigmoidal(DPPC)/polynom(DUPC) N_pl(T, Nc):\n")
+    if lipidtype == "DPPC":
+        f.write("# f(S) = Nmax + ( deltaN / (1+exp(-k(T-transitionT))) )\n")
+        f.write("#\t\t\t <Nmax> <deltaN> <k> <transitionT>\n")
+
+    sep = "\t"
+
+    try:
+        dat = pd.read_csv(parafilename, sep=sep)
+    except FileNotFoundError(f"{parafilename} not found"):
+        print("Missing parameter file for NLL(T, Nc) interaction.")
+        raise
+    
+    if len(dat.columns) == 1:
+        print("Failed to read NLL(T, Nc) file")
+        raise ValueError(f"Separator {sep} incorrect")
+
+    if lipidtype == "DPPC":
+        for Nc in range(5):
+            df = dat.loc[(dat.cholconc == CHOLCONC) & (dat.PL == lipidtype) & (dat.Nc == Nc)].drop(columns=["cholconc", "PL", "Nc"])
+            outstr = ' '.join(["LipidLipidNeighPara", lipidtype, _read_sigmoid_parafile(df), "\n"]) 
+            f.write(outstr)
+    elif lipidtype == "DUPC":
+        for Nc in range(5):
+            df = dat.loc[(dat.cholconc == CHOLCONC) & (dat.PL == lipidtype) & (dat.Nc == Nc)].drop(columns=["cholconc", "PL", "Nc"])
+            outstr = ' '.join(["LipidLipidNeighPara", lipidtype, _read_poly_parafile(df), "\n"]) 
+            f.write(outstr)
+    else:
+        raise ValueError(f"Unknown lipidtype: {lipidtype}")
+    f.write("\n")
+
+
+def add_NLC(f, lipidtype, parafilename="fitparameters_NofT_Nc_CHOL-{}.txt"):
+ 
+    parafilename = parafilename.format(lipidtype)
+    print("reading: ", parafilename)
+
+    f.write("#Chol-Lipid neighbor functions sigmoidal(DPPC)/polynom(DUPC) N_pl(T, Nc):\n")
+    if lipidtype == "DPPC":
+        f.write("# f(S) = Nmax + ( deltaN / (1+exp(-k(T-transitionT))) )\n")
+        f.write("#\t\t\t <Nmax> <deltaN> <k> <transitionT>\n")
+
+
+    sep = "\t"
+
+    try:
+        dat = pd.read_csv(parafilename, sep=sep)
+    except FileNotFoundError(f"{parafilename} not found"):
+        print("Missing parameter file for NLL(T, Nc) interaction.")
+        raise
+    
+    if len(dat.columns) == 1:
+        print("Failed to read NLL(T, Nc) file")
+        raise ValueError(f"Separator {sep} incorrect")
+
+    if lipidtype == "DPPC":
+        for Nc in range(5):
+            df = dat.loc[(dat.cholconc == CHOLCONC) & (dat.PL == lipidtype) & (dat.Nc == Nc)].drop(columns=["cholconc", "PL", "Nc"])
+            outstr = ' '.join(["CholLipidNeigh", lipidtype, _read_sigmoid_parafile(df), "\n"]) 
+            f.write(outstr)
+    elif lipidtype == "DUPC":
+        for Nc in range(5):
+            df = dat.loc[(dat.cholconc == CHOLCONC) & (dat.PL == lipidtype) & (dat.Nc == Nc)].drop(columns=["cholconc", "PL", "Nc"])
+            outstr = ' '.join(["CholLipidNeigh", lipidtype, _read_poly_parafile(df), "\n"]) 
+            f.write(outstr)
+    else:
+        raise ValueError(f"Unknown lipidtype: {lipidtype}")
+    f.write("\n")
+
+
+def add_entropy(f, parafilename="fitparameters_entropies.txt"):
     ''' '''
 
-def add_NLC():
-    ''' '''
-
-def add_entropy():
+def add_pscd(f, parafilename="fitparameter_pscd.txt"):
     ''' '''
 
 
@@ -203,15 +294,20 @@ def add_entropy():
 if __name__ == "__main__":
 
     with open(OUTFNAME, "w") as f:
-        f.write(f"# Parameters from c{CHOLCONC} and T{TEMPERATURE}\n")
+        f.write(f"# Parameters from c{CHOLCONC} and T{TEMPERATURE}\n\n")
 
         add_Eplpl(f)
         add_Eplc(f)
-        #add_Ecc(f)
-        #add_selfE(f)
-        #add_NLL(f)
-        #add_NLC(f)
+        add_Ecc(f)
+        add_selfE(f)
+        add_NLL(f, "DPPC")
+        add_NLC(f, "DPPC")
+        add_NLL(f, "DUPC")
+        add_NLC(f, "DUPC")
         #add_entropy(f)
+        #add_pscd(f)
+
+        f.write("\n")
 
     
 
